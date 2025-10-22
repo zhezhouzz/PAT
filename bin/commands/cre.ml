f9429cec7c505d7c411450fc9a08a8fe5ff9c601
aadi rave <arave@purdue.edu>
2025-10-22 14:04:11 -0400

    hashtable benchmark

--- bin/commands/cre.ml
+++ bin/commands/cre.ml
@@ -334,6 +334,12 @@ let test_eval s converge_bound () =
       let test () = Interpreter.once (init, main, check_membership_stack) in
       let _ = eval test in
       ()
+  | "hashtable" ->
+      let open Adt.Hashtable in
+      let main = Synthesis.load_progs s () in
+      let test () = Interpreter.once (init, main, check_membership_hashtable) in
+      let _ = eval test in
+      ()
   | "set" ->
       let open Adt.Set in
       let test () = Interpreter.once (init, [ main ], check_membership_set) in
