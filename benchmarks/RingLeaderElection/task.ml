c7cba97e62fa8410f71811b88ec015aaa3453827
zhouzhezz <zorichzorich@gmail.com>
2025-11-04 10:39:07 -0500

    fix cty rename error

--- benchmarks/RingLeaderElection/task.ml
+++ benchmarks/RingLeaderElection/task.ml
@@ -1,67 +1,35 @@
-(* type tNode = (node1 * node2[@tNode]) *)
+(* type tNode = tNode *)
 
 val ( == ) : 'a. 'a -> 'a -> bool
-val eWakeup : < node : (node1 * node2[@tNode]) > [@@gen]
+val next : tNode -> tNode
 
-val eNominate :
-  < node : (node1 * node2[@tNode]) ; leader : (node1 * node2[@tNode]) >
-[@@obs]
+let[@axiom] next2 (n : tNode) = next (next n) == n
+let[@axiom] next_diff (n : tNode) = not (next n == n)
 
-val eWon : < leader : (node1 * node2[@tNode]) > [@@obsRecv]
+val eWakeup : < node : tNode > [@@gen]
+val eNominate : < node : tNode ; leader : tNode > [@@obs]
+val eWon : < leader : tNode > [@@obsRecv]
 
-let eWakeup ?l:(n = (true : [%v: (node1 * node2[@tNode])])) =
+let eWakeup ?l:(n = (true : [%v: tNode])) =
   ( starA (anyA - EWon true),
     EWakeup (node == n),
     [| ENominate (node == n && leader == n) |] )
 
 let eNominate =
   [|
-    (fun ?l:(n =
-             (v == ("Node1" : (node1 * node2[@tNode]))
-               : [%v: (node1 * node2[@tNode])]))
-      ?l:(ld =
-          (v == ("Node2" : (node1 * node2[@tNode]))
-            : [%v: (node1 * node2[@tNode])]))
-    -> (allA, ENominate (leader == ld && node == n), [| EWon (leader == ld) |]));
-    (fun ?l:(n =
-             (v == ("Node1" : (node1 * node2[@tNode]))
-               : [%v: (node1 * node2[@tNode])]))
-      ?l:(ld =
-          (v == ("Node1" : (node1 * node2[@tNode]))
-            : [%v: (node1 * node2[@tNode])]))
-    ->
+    (fun ?l:(n = (true : [%v: tNode])) ?l:(ld = (not (v == n) : [%v: tNode])) ->
+      (allA, ENominate (leader == ld && node == n), [| EWon (leader == ld) |]));
+    (fun ?l:(n = (true : [%v: tNode])) ?l:(ld = (true : [%v: tNode])) ->
       ( allA,
-        ENominate (leader == ld && node == n),
-        [|
-          ENominate (leader == ld && node == ("Node2" : (node1 * node2[@tNode])));
-        |] ));
-    (fun ?l:(n =
-             (v == ("Node2" : (node1 * node2[@tNode]))
-               : [%v: (node1 * node2[@tNode])]))
-      ?l:(ld =
-          (v == ("Node1" : (node1 * node2[@tNode]))
-            : [%v: (node1 * node2[@tNode])]))
-    -> (allA, ENominate (leader == ld && node == n), [| EWon (leader == ld) |]));
-    (fun ?l:(n =
-             (v == ("Node2" : (node1 * node2[@tNode]))
-               : [%v: (node1 * node2[@tNode])]))
-      ?l:(ld =
-          (v == ("Node2" : (node1 * node2[@tNode]))
-            : [%v: (node1 * node2[@tNode])]))
-    ->
-      ( allA,
-        ENominate (leader == ld && node == n),
-        [|
-          ENominate (leader == ld && node == ("Node1" : (node1 * node2[@tNode])));
-        |] ));
+        ENominate (leader == ld && node == n && n == ld),
+        [| ENominate (leader == ld && node == next n) |] ));
   |]
 
-let eWon ?l:(ld = (true : [%v: (node1 * node2[@tNode])])) =
-  (allA, EWon (leader == ld), [||])
+let eWon ?l:(ld = (true : [%v: tNode])) = (allA, EWon (leader == ld), [||])
 
 (* unique leader *)
 
-let[@goal] task_RingLeaderElection (ld : (node1 * node2[@tNode])) =
+let[@goal] task_RingLeaderElection (ld : tNode) =
   allA;
   EWon (leader == ld);
   allA;
